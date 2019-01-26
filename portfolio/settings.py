ed94bd01c19fa79093592e62cda10814b9c9ea4a
fatihsuatk <fatihskabakci@gmail.com>
2019-01-26 20:06:06 +0300

    Blog functionality 3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'fatih0013',
+        'PASSWORD': 'portfolio1010',
         'HOST': 'localhost',
         'PORT': '5432',
     }
